45120a074b88db414ad914e8b64cf0b91b120a68
Cristian Caroli <cristiancaroli@gmail.com>
2016-12-17 21:11:45 +0100

    counting trips

--- src/models/Graph.js
+++ src/models/Graph.js
@@ -21,4 +21,25 @@ export class Graph {
         }
         return distance;
     }
+
+    countTrips(trip, location, goal, ceiling, comparator) {
+        const soFar = [...trip, location];
+        if (comparator(trip, ceiling) && location === goal)  {
+            return 1;
+        } else if (trip.length >= ceiling) {
+            return 0;
+        } else {
+            return [...this.nodes]
+                .filter((node) => node.start === location)
+                .reduce((sum, node) => sum + this.countTrips(soFar, node.end, goal, ceiling, comparator), 0);
+        }
+    };
+
+    countTripsWithExactStops(location, goal, stops) {
+        return this.countTrips([], location, goal, stops, (trip, ceiling) => trip.length === ceiling);
+    }
+
+    countTripsWithLessThanNStops(location, goal, stops) {
+        return this.countTrips([], location, goal, stops + 1, (trip, ceiling) => trip.length < ceiling  && trip.length > 1);
+    }
 }
--- test/graph-test.js
+++ test/graph-test.js
@@ -7,12 +7,24 @@ describe('Graph', () => {
     const graph = new Graph(parser('AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7'));
 
     describe('Calculating the Weight of a given route', () => {
-        it('should be 9', () => {
+        it('should be the sum of the weights of all the edges in the route or NO SUCH ROUTE if there is none', () => {
             assert.equal(graph.weightOfPath('A-B-C'), 9);
             assert.equal(graph.weightOfPath('A-D'), 5);
             assert.equal(graph.weightOfPath('A-D-C'), 13);
             assert.equal(graph.weightOfPath('A-E-B-C-D'), 22);
             assert.equal(graph.weightOfPath('A-E-D'), 'NO SUCH ROUTE');
+        });
+    });
+
+    describe('Calculating the number of trips starting and ending at the same place with exactly N stops', () => {
+        it('Should be only a fixed amount of routes', () => {
+            assert.equal(graph.countTripsWithExactStops('A', 'C', 4), 3);
+        });
+    });
+
+    describe('Calculating the number of trips starting and ending at the same place with at least N stops', () => {
+        it('Should be only a fixed amount of routes', () => {
+            assert.equal(graph.countTripsWithLessThanNStops('C', 'C', 3), 2);
         });
     });
 });
